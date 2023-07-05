b857dedfe886ac59626ad5d29217d733aa6fb8d7
flebo45 <alessio.torrieri@gmail.com>
2023-07-05 16:36:28 +0200

    Added composer.lock

--- Foundation/FLike.php
+++ Foundation/FLike.php
@@ -6,6 +6,21 @@ class FLike{
 
     private $table_field = "id";
 
-    
+     # methods
+
+     public static function getTable(){
+
+        return self::$table_name;
+    }
+
+    public static function getField(){
+
+        return self::$table_field;
+    }
+
+    public static function createLikeInDB(){
+
+        
+    }
 
 }
