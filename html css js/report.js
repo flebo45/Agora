68d10e8b843e11a7efd7e6dec1a6f672bf909a67
Marththt <alessio.martinelli1@student.univaq.it>
2023-07-07 19:18:25 +0200

    add modal in profile for: edit,report and delete

--- html css js/report.js
+++ html css js/report.js
@@ -1,17 +1,61 @@
+// MODAL FOR THEME
 
-
-const report=document.querySelector('#report');
-const reportModal=document.querySelector('.report');
+const report = document.querySelector('#report');
+const reportModal1 = document.querySelector('.report')
 
 const openReportModal =() => {
-    reportModal.style.display = 'grid'
+    reportModal1.style.display = 'grid'
 }
 
 const closeReportModal=(e)=>{
     if(e.target.classList.contains('report')){
-        reportModal.style.display =  'none';
+        themeModal1.style.display =  'none';
     }
 }
 //CLOSE MODAL
-reportModal.addEventListener('click', closeReportModal)
+reportModal1.addEventListener('click', closeReportModal)
 report.addEventListener('click', openReportModal)
+//FONTS
+
+
+//MODAL FOR EDIT
+
+const edit = document.querySelector('#edit');
+const editModal1 = document.querySelector('.edit')
+
+const openEditModal =() => {
+    editModal1.style.display = 'grid'
+}
+
+const closeEditModal=(e)=>{
+    if(e.target.classList.contains('edit')){
+        editModal1.style.display =  'none';
+    }
+}
+//CLOSE MODAL
+editModal1.addEventListener('click', closeEditModal)
+edit.addEventListener('click', openEditModal)
+//FONTS
+
+
+
+//MODAL FOR DELETE
+
+const delete1 = document.querySelector('#delete');
+const deleteModal1 = document.querySelector('.delete')
+
+const openDeleteModal =() => {
+    deleteModal1.style.display = 'grid'
+}
+
+const closeDeleteModal=(e)=>{
+    if(e.target.classList.contains('delete')){
+        deleteModal1.style.display =  'none';
+    }
+}
+//CLOSE MODAL
+deleteModal1.addEventListener('click', closeDeleteModal)
+delete1.addEventListener('click', openDeleteModal)
+//FONTS
+
+
