0448d4c9cc655809ccbdb91830bd065cc274eedc
SilviaMastracci <126976603+SilviaMastracci@users.noreply.github.com>
2023-07-08 16:36:48 +0200

    smarty

---
+++ StartSmarty.php
@@ -0,0 +1,13 @@
+<?php
+require('Smarty/Smarty.class.php');
+
+class StartSmarty{
+    static function configuration(){
+        $smarty=new Smarty();
+        $smarty->template_dir='Smarty/templates/';
+        $smarty->compile_dir='Smarty/templates_c/';
+        $smarty->config_dir='Smarty/configs/';
+        $smarty->cache_dir='Smarty/cache/';
+        return $smarty;
+    }
+}
