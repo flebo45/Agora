edc9e36fc7486edb87279d40d79f6ed3dcff9e64
flebo45 <alessio.torrieri@gmail.com>
2023-09-10 20:13:02 +0200

    Deleted smarty

--- StartSmarty.php
+++ StartSmarty.php
@@ -4,10 +4,10 @@ require('Smarty/Smarty.class.php');
 class StartSmarty{
     static function configuration(){
         $smarty=new Smarty();
-        $smarty->template_dir='Smarty/templates/';
-        $smarty->compile_dir='Smarty/templates_c/';
-        $smarty->config_dir='Smarty/configs/';
-        $smarty->cache_dir='Smarty/cache/';
+        $smarty->template_dir='src/templates/';
+        $smarty->compile_dir='src/templates_c/';
+        $smarty->config_dir='src/configs/';
+        $smarty->cache_dir='src/cache/';
         return $smarty;
     }
 }
